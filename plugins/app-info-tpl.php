a51224db477ddf03f805b33878a705cc79e3587c
Tomas Doubek <thomas.doubek@gmail.com>
2019-06-26 16:13:28 +0200

    button app info, routing to app info

--- plugins/app-info-tpl.php
+++ plugins/app-info-tpl.php
@@ -2,7 +2,10 @@
 	<img src="<?php echo $data['article']['img']?>" >
 	<div class="text-buble">
 	   <h3><?php echo $data['article']['perex']?> - <strong class="color-blue-light"><?php echo $data['headline']?></strong></h3>
+	   <a href="<?php echo "#departments-tpl_main"?>" class="button button-round button-blue profile-page-button-1"><?php echo MultyLanguage::translate($data, "departments", "translate");?></a>
+		
 	   <?php echo $data['article']['content']?>
 	   <div class="decoration"></div>
+	  	 
 	</div>
  </div>
